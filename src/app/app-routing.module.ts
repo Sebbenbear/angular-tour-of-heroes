bb69c159f4061df8072bf4b09c79236415b72e5a
Pauline Kelly <pauline.m.kelly1@gmail.com>
2018-01-25 17:47:37 +1100

    Add routing to Heroes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
+
+const routes : Routes = [
+  { path: 'heroes', component: HeroesComponent }
+]
 
 @NgModule({
-  exports: [
-    RouterModule
+  exports: [ RouterModule ],
+  imports: [
+    RouterModule.forRoot(routes) // forRoot is configured at the apps root lvel. gives service providers and directives for routing, initial nav.
   ]
 })
 
