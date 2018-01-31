fe58ffa92dbc8a4e28acd83197a34faba6192bd2
Pauline Kelly <pauline.kelly@ericsson.com>
2018-01-31 11:49:46 +1100

    Add material ui for textfield in heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './/app-routing.module';
 
 import { MatButtonModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataService }  from './in-memory-data.service';
@@ -28,7 +29,8 @@ import { HeroSearchComponent } from './hero-search/hero-search.component';
     HttpClientInMemoryWebApiModule.forRoot(
       InMemoryDataService, { dataEncapsulation: false }
     ),
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule
   ],
   declarations: [
     AppComponent,
